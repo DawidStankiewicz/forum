80436701d83f0bf2618be072a299d9a0d655a4ab
Dawid Stankiewicz <theszczypiorek@gmail.com>
2016-07-10 21:26:38 +0200

    Spring Security basic configuration

---
+++ src/main/java/com/github/szczypioreg/forum/config/security/SecurityConfig.java
@@ -0,0 +1,24 @@
+/**
+ * Created by Dawid Stankiewicz on 10.07.2016
+ */
+package com.github.szczypioreg.forum.config.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    
+    protected void configure(HttpSecurity http) throws Exception {
+        http.formLogin().and().authorizeRequests().antMatchers("/user/**").hasRole("USER");
+    }
+    
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("admin").password("pass").roles("ADMIN", "USER")
+                .and().withUser("user").password("pass").roles("USER");
+    }
+}
