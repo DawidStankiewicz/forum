4f7fc56a9d623589659ff3515224a67f214b67e6
Dawid Stankiewicz <theszczypiorek@gmail.com>
2016-07-10 23:06:08 +0200

    Added UserController and logout function

---
+++ src/main/java/com/github/szczypioreg/forum/controller/UserController.java
@@ -0,0 +1,28 @@
+/**
+ * Created by Dawid Stankiewicz on 10.07.2016
+ */
+package com.github.szczypioreg.forum.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UserController {
+    
+    @RequestMapping("/logout")
+    public String logoutPage(HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null) {
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+            
+        }
+        return "redirect:/login?logout";
+    }
+    
+}
