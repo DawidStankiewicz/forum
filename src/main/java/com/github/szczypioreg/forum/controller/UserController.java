edeffcdad8b48f6411003d3e404bb9e499d8cfcb
Dawid Stankiewicz <theszczypiorek@gmail.com>
2016-07-11 19:54:09 +0200

    Test mapping for /users and /regiristration

--- src/main/java/com/github/szczypioreg/forum/controller/UserController.java
+++ src/main/java/com/github/szczypioreg/forum/controller/UserController.java
@@ -3,17 +3,49 @@
  */
 package com.github.szczypioreg.forum.controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.github.szczypioreg.forum.domain.User;
+import com.github.szczypioreg.forum.service.UserService;
 
 @Controller
 public class UserController {
+    
+    @Autowired
+    private UserService userService;
+    
+    @RequestMapping("/users")
+    public String allUsersPage(Model model) {
+        List<User> users = userService.getAllUsers();
+        model.addAttribute("users", users);
+        return "users";
+    }
+    
+    @RequestMapping(value = "/regiristration", method = RequestMethod.GET)
+    public String regiristrationForm(Model model) {
+        User user = new User();
+        model.addAttribute("userModel", user);
+        return "regiristration";
+    }
+    
+    @RequestMapping(value = "/regiristration", method = RequestMethod.POST)
+    public String regiristrationNewUser(@ModelAttribute("newUser") User user) {
+        userService.add(user);
+        return "redirect:/user/" + user.getName();
+    }
     
     @RequestMapping("/logout")
     public String logoutPage(HttpServletRequest request, HttpServletResponse response) {
