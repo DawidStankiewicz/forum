38d0e8f299ed8d7098435a19e99191c869585661
Dawid Stankiewicz <theszczypiorek@gmail.com>
2016-07-17 18:36:23 +0200

    Section model

---
+++ src/main/java/com/github/szczypioreg/forum/domain/Section.java
@@ -0,0 +1,83 @@
+/**
+ * Created by Dawid Stankiewicz on 17.07.2016
+ */
+package com.github.szczypioreg.forum.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "sections")
+public class Section implements Serializable {
+    
+    private static final long serialVersionUID = 4309596024036692836L;
+    
+    @Id
+    @Column(name = "idsection")
+    private int idSection;
+    
+    @Column(name = "name")
+    private String name;
+    
+    public Section() {}
+    
+    public Section(int idSection, String name) {
+        super();
+        this.idSection = idSection;
+        this.name = name;
+    }
+    
+    public int getIdSection() {
+        return idSection;
+    }
+    
+    public void setIdSection(int idSection) {
+        this.idSection = idSection;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + idSection;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        return result;
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Section other = (Section) obj;
+        if (idSection != other.idSection)
+            return false;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        return true;
+    }
+    
+    @Override
+    public String toString() {
+        return "Section [idSection=" + idSection + ", name=" + name + "]";
+    }
+    
+}
