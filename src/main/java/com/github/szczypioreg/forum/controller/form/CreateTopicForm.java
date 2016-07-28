cd7e95f5687cc64be5b4fa84243c7a08c236de19
Dawid Stankiewicz <theszczypiorek@gmail.com>
2016-07-28 14:30:53 +0200

    Created models for forms

---
+++ src/main/java/com/github/szczypioreg/forum/controller/form/CreateTopicForm.java
@@ -0,0 +1,40 @@
+/**
+ * Created by Dawid Stankiewicz on 28.07.2016
+ */
+package com.github.szczypioreg.forum.controller.form;
+
+public class CreateTopicForm {
+	
+	private String title;
+	
+	private String content;
+	
+	private int sectionId;
+
+	public CreateTopicForm() {}
+	
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public int getSectionId() {
+		return sectionId;
+	}
+
+	public void setSectionId(int sectionId) {
+		this.sectionId = sectionId;
+	}
+	
+}
---
+++ src/main/java/com/github/szczypioreg/forum/controller/form/UserRegistrationForm.java
@@ -0,0 +1,40 @@
+/**
+ * Created by Dawid Stankiewicz on 28.07.2016
+ */
+package com.github.szczypioreg.forum.controller.form;
+
+public class UserRegistrationForm {
+	
+	private String email;
+	
+	private String username;
+	
+	private String password;
+	
+	public UserRegistrationForm() {}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+}
