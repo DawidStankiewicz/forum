d54e1a3f902c62987ceeaae7ccaec3f29b7b57a4
Dawid Stankiewicz <theszczypiorek@gmail.com>
2016-07-30 11:57:09 +0200

    Added form for create new section

--- src/main/java/com/github/szczypioreg/forum/controller/SectionController.java
+++ src/main/java/com/github/szczypioreg/forum/controller/SectionController.java
@@ -6,20 +6,45 @@ package com.github.szczypioreg.forum.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.github.szczypioreg.forum.controller.form.NewSectionForm;
+import com.github.szczypioreg.forum.domain.Section;
+import com.github.szczypioreg.forum.service.SectionService;
 import com.github.szczypioreg.forum.service.TopicService;
+
 
 @Controller
 public class SectionController {
     
     @Autowired
+    private SectionService sectionService;
+    
+    @Autowired
     private TopicService topicService;
     
-    @RequestMapping("/section/{section}")
-    public String getTopicsFromSection(@PathVariable String section, Model model) {
-        model.addAttribute("topics", topicService.findBySection(section));
+    @RequestMapping("/section/{id}")
+    public String getTopicsFromSection(@PathVariable int id, Model model) {
+        model.addAttribute("topics", topicService.findBySection(id));
         return "section";
     }
+    
+    @RequestMapping(value = "/section/new", method = RequestMethod.GET)
+    public String getNewSectionForm(Model model) {
+        model.addAttribute("newSection", new NewSectionForm());
+        return "new_section_form";
+    }
+    
+    @RequestMapping(value = "/section/new", method = RequestMethod.POST)
+    public String processAndAddNewSection(@ModelAttribute("newSection") NewSectionForm newSection,
+            Model model) {
+        Section section = new Section();
+        section.setName(newSection.getName());
+        section = sectionService.save(section);
+        return "redirect:/section/" + section.getIdSection();
+    }
+    
 }
