fb57007efd574a51746c6abd9d98fbeff3b0ec76
Dawid Stankiewicz <theszczypiorek@gmail.com>
2016-07-30 11:55:34 +0200

    Model class for new section form

---
+++ src/main/java/com/github/szczypioreg/forum/controller/form/NewSectionForm.java
@@ -0,0 +1,20 @@
+/**
+ * Created by Dawid Stankiewicz on 29.07.2016
+ */
+package com.github.szczypioreg.forum.controller.form;
+
+public class NewSectionForm {
+    
+    private String name;
+    
+    public NewSectionForm() {}
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+}
