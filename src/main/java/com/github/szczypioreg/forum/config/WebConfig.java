f1fe8f0e7659a2d5bb806bf81d5371ef8832c925
Dawid Stankiewicz <theszczypiorek@gmail.com>
2016-07-30 12:01:55 +0200

    Deleted void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer)

--- src/main/java/com/github/szczypioreg/forum/config/WebConfig.java
+++ src/main/java/com/github/szczypioreg/forum/config/WebConfig.java
@@ -5,21 +5,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
-import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+
 @EnableWebMvc
 @Configuration
 @ComponentScan("com.github.szczypioreg.forum")
 public class WebConfig extends WebMvcConfigurerAdapter {
-    
-    @Override
-    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
-        configurer.enable();
-    }
     
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
@@ -39,4 +34,5 @@ public class WebConfig extends WebMvcConfigurerAdapter {
         messageSource.setDefaultEncoding("UTF-8");
         return messageSource;
     }
+    
 }
