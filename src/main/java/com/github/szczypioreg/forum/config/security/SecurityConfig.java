67494758609d3b9df151b6723790928ce9ec3adc
Dawid Stankiewicz <theszczypiorek@gmail.com>
2017-07-30 13:12:56 +0200

    Convert an application to Spring Boot
    - remove all dependencies and add new Spring Boot starters
    - remove config classes
    - add Application class with main method
    - move thymeleaf templates to resources/templates
    - fix end tags in templates '/>'
    - add application.properties with DB, Hibernate and Thymeleaf config
    
    errors with:
    - login page - 404 at /login
    - messages on home template - section and new section button
    - security conditions in templates - login and logout buttons

--- src/main/java/com/github/szczypioreg/forum/config/security/SecurityConfig.java
+++ src/main/java/com/github/szczypioreg/forum/config/security/SecurityConfig.java
@@ -19,55 +19,55 @@ import org.springframework.web.filter.CharacterEncodingFilter;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    
+
     @Autowired
     private DataSource dataSource;
-    
+
     @Autowired
     private PasswordEncoder passwordEncoder;
-    
+
     private static final String USERS_BY_USERNAME_QUERY =
-        "select username, password, is_active from users where username=?";
-    
+            "select username, password, is_active from users where username=?";
+
     private static final String AUTHORIZES_BY_USERNAME_QUERY =
-        "select users.username, concat('ROLE_', roles.name) from users, roles, rolesofusers "
-                + "where users.username=? and users.id=forum.rolesofusers.id_user and rolesofusers.id_role=roles.id";
-    
-    private static final String[] PAGES_ONLY_FOR_AUTHORIZED_USERS = { "/user/**",
-                                                                      "/topic/new/**",
-                                                                      "/topic/delete/**",
-                                                                      "/section/delete/**",
-                                                                      "/section/new/**",
-                                                                      "/post/**" };
-    
-    private static final String[] LIST_OF_PAGES_ONLY_FOR_ADMINS = { "/admin/**",
-                                                                    "/users/**",
-                                                                    "/section/new" };
-    
-    private static final String[] LIST_OF_AUTHORIZED_ROLES = { "USER",
-                                                               "ADMIN" };
-    
-    private static final String[] LIST_OF_ADMINS_ROLES = { "HEAD_ADMIN",
-                                                           "ADMIN" };
-    
+            "select users.username, concat('ROLE_', roles.name) from users, roles, rolesofusers "
+                    + "where users.username=? and users.id=forum.rolesofusers.id_user and rolesofusers.id_role=roles.id";
+
+    private static final String[] PAGES_ONLY_FOR_AUTHORIZED_USERS = {"/user/**",
+            "/topic/new/**",
+            "/topic/delete/**",
+            "/section/delete/**",
+            "/section/new/**",
+            "/post/**"};
+
+    private static final String[] LIST_OF_PAGES_ONLY_FOR_ADMINS = {"/admin/**",
+            "/users/**",
+            "/section/new"};
+
+    private static final String[] LIST_OF_AUTHORIZED_ROLES = {"USER",
+            "ADMIN"};
+
+    private static final String[] LIST_OF_ADMINS_ROLES = {"HEAD_ADMIN",
+            "ADMIN"};
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.formLogin().loginPage("/login").and().rememberMe().tokenValiditySeconds(2419200).key(
                 "f0rumKey").and().logout().and().authorizeRequests().antMatchers(
-                        PAGES_ONLY_FOR_AUTHORIZED_USERS).hasAnyRole(LIST_OF_AUTHORIZED_ROLES)
+                PAGES_ONLY_FOR_AUTHORIZED_USERS).hasAnyRole(LIST_OF_AUTHORIZED_ROLES)
                 .antMatchers(LIST_OF_PAGES_ONLY_FOR_ADMINS).hasAnyRole(LIST_OF_ADMINS_ROLES);
-        
+
         CharacterEncodingFilter filter = new CharacterEncodingFilter();
         filter.setEncoding("UTF-8");
         filter.setForceEncoding(true);
         http.addFilterBefore(filter, CsrfFilter.class);
     }
-    
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.jdbcAuthentication().dataSource(dataSource).usersByUsernameQuery(
                 USERS_BY_USERNAME_QUERY).authoritiesByUsernameQuery(AUTHORIZES_BY_USERNAME_QUERY)
                 .passwordEncoder(passwordEncoder);
     }
-    
+
 }
