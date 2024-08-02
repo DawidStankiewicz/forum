6421309ca5cae0f27c4cda416cdc9cbda5164829
Dawid Stankiewicz <theszczypiorek@gmail.com>
2024-08-02 23:53:22 +0200

    fix new section form
    - replace default head with head-bootstrap
    - fix data.sql when running the app second time

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,8 @@
+DELETE FROM forum.sections;
+DELETE FROM forum.user_roles;
+DELETE FROM forum.roles;
+DELETE FROM forum.users;
+
 INSERT INTO forum.users (id, createdAt, email, emailToken, enabled, lastLoginTime, password, removed, secondaryEmail, username) VALUES (1, '2024-08-01 13:26:41.657118', 'admin@admin.com', '3faeca896e0cb050', true, null, '$2a$10$rNQGG7DQcCNYl3Lp/1V/XuOgAPFhNVX3C2Kc3BUFYoGfi.1ENc45i', false, null, 'admin');
 
 INSERT INTO forum.roles (id, name) VALUES (1, 'ADMIN');
